b6d326b8af5719a7ac82b3b3eb4797db758d26d0
vancomm <vanushnick@gmail.com>
2021-07-20 23:38:03 +0300

    Added linter

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { name } from '../src/cli.js';
+import name from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
-console.log('Hello, ' + name + '!');
+console.log(`Hello, ${name}!`);
--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,3 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
-export const name = readlineSync.question('May I have your name? ');
+export default readlineSync.question('May I have your name? ');

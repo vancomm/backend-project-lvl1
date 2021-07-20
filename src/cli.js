cd81830b6c54c2ae64c0dbea80bc407273d48c1a
vancomm <vanushnick@gmail.com>
2021-07-21 02:21:00 +0300

    Fixed brain-game

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-  const name = readlineSync.question('May I have your name? ');
-
   console.log('Welcome to the Brain Games!');
+
+  const name = readlineSync.question('May I have your name? ');
 
   console.log(`Hello, ${name}!`);
 };

aeb44811d57c006fc4dd4eb8e95bd9d7f47f7da2
vancomm <vanushnick@gmail.com>
2021-07-21 11:55:47 +0300

    Added gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-param-reassign */
+import logic from './index.js';
+import { getRandomInt } from '../random.js';
+
+const gcd = (a, b) => {
+  if (a !== 0 && b !== 0) {
+    if (a > b) a %= b;
+    else b %= a;
+    return gcd(a, b);
+  }
+  return a + b;
+};
+
+const generateQuestion = () => {
+  const a = getRandomInt(1, 50);
+  const b = getRandomInt(1, 50);
+  const question = [a, b].join(' ');
+  const correctAnswer = gcd(a, b).toString();
+  return [question, correctAnswer];
+};
+
+export default () => {
+  const rules = 'Find the greatest common divisor of given numbers.';
+  logic(rules, generateQuestion);
+};

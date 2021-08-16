985d1f6e74ca56f0ff1041d8e4da357b11df36be
vancomm <vanushnick@gmail.com>
2021-08-16 14:26:52 +0300

    Fixed calc game: added subtraction

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,12 +1,25 @@
 import logic from './index.js';
 import { getRandom, getRandomInt } from '../random.js';
 
+const getOperation = () => {
+  const random = getRandom(0, 1);
+  if (random < 0.3) return '+';
+  if (random < 0.6) return '-';
+  return '*';
+};
+
+const getResult = (a, b, op) => {
+  if (op === '+') return a + b;
+  if (op === '-') return a - b;
+  return a * b;
+};
+
 const generateQuestion = () => {
-  const op = getRandom(0, 1) > 0.5 ? '+' : '*';
+  const op = getOperation();
   const a = getRandomInt(1, 30);
   const b = getRandomInt(1, 30);
   const question = [a, op, b].join(' ');
-  const correctAnswer = (op === '+' ? a + b : a * b).toString();
+  const correctAnswer = getResult(a, b, op).toString();
   return [question, correctAnswer];
 };
 
